4887676a3e366f81786d1f96b3b1bbbaaaec7e6c
Charlie <stcarloscardona@gmai.com>
2026-01-31 14:18:20 -0500

    funcionalidad agregada

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Voy para la seguna clase");
         System.out.println("Netflix me mira con ojos de desprecio");
+        System.out.println("modificación para la nueva rama");
+        System.out.println("funcionalidad agregada");
     }
 }
