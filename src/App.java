b8a8cca9f3cb2a106ef8ff133718aaea060268d4
Charlie <stcarloscardona@gmai.com>
2026-01-31 13:41:56 -0500

    second feature agree

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Voy para la seguna clase");
+        System.out.println("Netflix me mira con ojos de desprecio");
     }
 }
